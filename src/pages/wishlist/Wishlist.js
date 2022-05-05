8e73cd594111b482fd0a236a82dae172ebb22897
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2022-05-05 19:41:28 +0530

    feat: added wishlist , cart,checkout

--- src/pages/wishlist/Wishlist.js
+++ src/pages/wishlist/Wishlist.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { WishlistProduct } from '../../components'
+import { useCartContext } from '../../context/cartWishlistContext'
+import '../wishlist/wishlist.css'
 function Wishlist() {
+  const {cartState,cartDispatch}=useCartContext()
+  const{wishlist}=cartState
+ 
   return (
-    <div>Wishlist</div>
+<div className='wishlist-page'>
+<h2>My Wishlist ({wishlist.length})</h2>
+<div className='wishlist-layout'>
+{wishlist.map((product)=>{
+  return  <WishlistProduct key={product.id} {...product}/>
+})}
+</div>
+</div>  
   )
 }
 
