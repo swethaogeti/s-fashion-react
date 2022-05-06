d36860ff0828a77d869e619e324f749ce41d34b3
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2022-05-06 23:05:01 +0530

    fix: solved a minor bug

--- src/components/wishlistCard/WishlistProduct.js
+++ src/components/wishlistCard/WishlistProduct.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../wishlistCard/wishlistProduct.css'
 import { useCartContext } from '../../context/cartWishlistContext'
-const WishlistProduct = ({image,brand,id,title,price,discount,total_price,amount}) => {
+const WishlistProduct = ({image,brand,id,title,price,discount,total_price}) => {
     const {cartState,cartDispatch}=useCartContext()
-    const {cart,whishlist,total}=cartState
+    const {cart,whishlist,total,amount}=cartState
     
   return (
     <div className='wishlist-prd product-card'>
@@ -16,7 +16,7 @@ const WishlistProduct = ({image,brand,id,title,price,discount,total_price,amount
  {cart.some((i)=>i.id===id)
     ?(<div className='badge text-badge icon-badge active-icon'><i className='fas fa-shopping-cart' ></i></div>)
     
- :(<div className='badge text-badge icon-badge'><i className='fas fa-shopping-cart' onClick={()=>cartDispatch({type:'ADD_TO_CART',item:{image,brand,title,id,price,discount,total_price,amount}})} ></i></div>)}
+ :(<div className='badge text-badge icon-badge'><i className='fas fa-shopping-cart' onClick={()=>{cartDispatch({type:'ADD_TO_CART',item:{image,brand,title,id,price,discount,total_price,amount:1}}) }} ></i></div>)}
  
 
  <div className='badge text-badge icon-badge-trash'><i className='fa fa-trash' onClick={()=>{cartDispatch({type:'REMOVE_FROM_WISHLIST',item:id})}}></i></div>
